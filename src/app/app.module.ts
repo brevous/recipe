c5d7d2f2fbf1eb5b3b0c7ca2d42b8ae976d86c0e
brev <armistead.michael@gmail.com>
2017-10-16 14:12:42 -0500

    login / logout / authguard (route protection) all working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
+import { AuthGuardService } from './auth/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,7 @@ import { AuthService } from './auth/auth.service';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ ListService, RecipeService, DataStorageService, AuthService ],
+  providers: [ ListService, RecipeService, DataStorageService, AuthService, AuthGuardService ],
   bootstrap: [ AppComponent ]
 })
 
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import * as firebase from 'firebase';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
-
+@Injectable()
 export class AuthService {
   token: string;
+
+  constructor(private router: Router) {
+  }
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -15,6 +20,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.router.navigate([ '/' ]);
           firebase.auth().currentUser.getToken()
             .then(
               (token: string) => this.token = token
@@ -33,5 +39,14 @@ export class AuthService {
         (token: string) => this.token = token
       );
     return this.token;
+  }
+
+  isAuthenticated() {
+    return this.token != null;
+  }
+
+  logout() {
+    firebase.auth().signOut();
+    this.token = null;
   }
 }
